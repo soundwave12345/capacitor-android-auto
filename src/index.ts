ebf1317ef8f81ec4cc7d82da975f800ed5dc8dec
soundwave12345 <evgueni.t@gmail.com>
2025-12-01 16:46:05 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { registerPlugin } from '@capacitor/core';
+
+import type { AndroidAutoPlugin } from './definitions';
+
+const AndroidAuto = registerPlugin<AndroidAutoPlugin>('AndroidAuto', {
+  web: () => import('./web').then(m => new m.AndroidAutoWeb()),
+});
+
+export * from './definitions';
+export { AndroidAuto };
