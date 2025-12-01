b99e0be903e96a382282e387782e49ec27156c82
soundwave12345 <evgueni.t@gmail.com>
2025-12-01 16:46:32 +0100

    Create web.ts

---
+++ src/web.ts
@@ -0,0 +1,20 @@
+import { WebPlugin } from '@capacitor/core';
+
+import type { AndroidAutoPlugin, PlayerState } from './definitions';
+
+export class AndroidAutoWeb extends WebPlugin implements AndroidAutoPlugin {
+  async updatePlayerState(options: PlayerState): Promise<void> {
+    console.log('updatePlayerState', options);
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async startService(): Promise<void> {
+    console.log('startService');
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async stopService(): Promise<void> {
+    console.log('stopService');
+    throw this.unimplemented('Not implemented on web.');
+  }
+}
